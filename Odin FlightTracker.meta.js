125055e376fe5cdcd76321bc19d2b9340de05746
BjornOdinsson89 <bjornodinsson89@gmail.com>
2026-03-13 11:39:42 -0600

    Update version and URLs in meta file

--- Odin FlightTracker.meta.js
+++ Odin FlightTracker.meta.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Odin FlightTracker v1.0.3
-// @version      1.0.3
+// @version      1.0.4
 // @description  Flight Tracking
 // @author       BjornOdinsson89
 // @match        https://www.torn.com/*
 // @grant        GM_getValue
 // @grant        GM_setValue
-// @downloadURL  https://github.com/bjornodinsson89/odin-flight-tracker/main/Odin%20FlightTracker%20v1.0.1-1.0.1.user.js
-// @updateURL    https://github.com/bjornodinsson89/odin-flight-tracker/main/Odin%20FlightTracker%20v1.0.1-1.0.1.meta.js
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @downloadURL  https://raw.githubusercontent.com/bjornodinsson89/odin-flight-tracker/main/Odin%20FlightTracker%20v1.0.4.user.js
+// @updateURL    https://raw.githubusercontent.com/bjornodinsson89/odin-flight-tracker/main/Odin%20FlightTracker%20v1.0.4.user.js
 // @run-at       document-idle
 // ==/UserScript==
