93527528fcf0ccec744838aa92296945e4d9e208
BjornOdinsson89 <bjornodinsson89@gmail.com>
2026-03-12 21:31:02 -0600

    Create Odin FlightTracker metadata file
    Added metadata for Odin FlightTracker user script.

---
+++ Odin FlightTracker.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Odin FlightTracker v1.0.3
+// @version      1.0.3
+// @description  Flight Tracking
+// @author       BjornOdinsson89
+// @match        https://www.torn.com/*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @downloadURL  https://raw.githubusercontent.com/bjornodinsson89/odin-flight-tracker/main/Odin%20FlightTracker%20v1.0.1-1.0.1.user.js
+// @updateURL    https://raw.githubusercontent.com/bjornodinsson89/odin-flight-tracker/main/Odin%20FlightTracker%20v1.0.1-1.0.1.meta.js
+// @run-at       document-idle
+// ==/UserScript==
