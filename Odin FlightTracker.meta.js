a71a5a0ff3601276842385749dba6716bd4a3535
BjornOdinsson89 <bjornodinsson89@gmail.com>
2026-04-08 08:40:57 -0600

    Update Odin FlightTracker.meta.js

--- Odin FlightTracker.meta.js
+++ Odin FlightTracker.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Odin FlightTracker
-// @version      1.0.7
+// @version      1.0.8
 // @description  Flight Tracking
 // @author       BjornOdinsson89
 // @icon         https://i.postimg.cc/BQ6bSYKM/file-000000004bb071f5a96fc52564bf26ad-(1).png
